967f232f9f8848e44901b95158fe81ad9ee591ff
Lisias T <me@lisias.net>
2024-07-25 14:59:44 -0300

    **FINALLY** scrapping images and styles exactly how I wanted!

--- Source/ARCHIVE/forum.kerbalspaceprogram.com/spider/doit.py
+++ Source/ARCHIVE/forum.kerbalspaceprogram.com/spider/doit.py
@@ -11,6 +11,7 @@ class MySpider(CrawlSpider):
 	name = 'forum.kerbalspaceprogram.com'
 	allowed_domains = ['forum.kerbalspaceprogram.com', 'kerbal-forum-uploads.s3.us-west-2.amazonaws.com']
 	start_urls = ['https://forum.kerbalspaceprogram.com/']
+
 	custom_settings = {
 		'AUTOTHROTTLE_ENABLED': True,
 		'AUTOTHROTTLE_START_DELAY': 1,
@@ -21,6 +22,10 @@ class MySpider(CrawlSpider):
 		'DOWNLOADER_MIDDLEWARES': {
 			'crawler.CustomProxyMiddleware': 350,
 		},
+		'ITEM_PIPELINES': {
+			"crawler.DevNullFilesPipeline": 1, 
+		},
+		'FILES_STORE' : "./FILE_STORE",
 		'DUPEFILTER_CLASS': 'crawler.SeenURLFilter',
 		'USER_AGENT': "Mozilla/5.0 (Linux; x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36",
 		'DEFAULT_REQUEST_HEADERS': {
@@ -33,14 +38,12 @@ class MySpider(CrawlSpider):
 	)
 
 	def parse_item(self, response):
-#		print(response.url)
-		urls = response.css('link::attr(href)').getall()
+		print(response.url)
+		urls = list()
+		#urls += response.css('link::attr(href)').getall()
 		urls += response.css('script::attr(src)').getall()
 		urls += response.css('img::attr(src)').getall()
 		for link in urls:
-#			print (link)
-			if not link.startswith("http"):
-				link = f"https:{link}"
-			yield {'url': link}
+			print (link)
+			yield { 'file_urls': [response.urljoin(link)] }
 		yield {'url': response.url}
-
