16a77ddd118877eb466c00e1e660349f839e8721
Lisias T <me@lisias.net>
2024-08-27 03:39:22 -0300

    Yet better charts

--- torrent/reports/report_chart.py
+++ torrent/reports/report_chart.py
@@ -37,9 +37,9 @@ def cvs_load_complaints(name:str) -> dict:
 			r[kind][timestamp] = count
 	return r
 
-def normalize_dataset_hourly(dataset:dict, target:datetime.datetime) -> dict:
+def __normalize_dataset(dataset:dict, target:tuple, increment:callable) -> dict:
 	for d in dataset:
-		dataset[d] = { k:v for k, v in dataset[d].items() if k >= target }
+		dataset[d] = { k:v for k, v in dataset[d].items() if k >= target[0] and k <= target[1] }
 
 	all_keys = set()
 	for i in dataset:
@@ -50,52 +50,34 @@ def normalize_dataset_hourly(dataset:dict, target:datetime.datetime) -> dict:
 	while d < end:
 		if d not in all_keys:
 			all_keys.add(d)
-		d = d + datetime.timedelta(hours=1)
+		d = increment(d)
 
 	for i in all_keys:
 		for j in dataset:
 			if i not in dataset[j]: dataset[j][i]=0
 	return dataset
+    
+def normalize_dataset_hourly(dataset:dict, target:tuple) -> dict:
+        return __normalize_dataset(dataset, target, lambda d: d + datetime.timedelta(hours=1) )
 				
 def normalize_dataset_minutely(dataset:dict, target:datetime.datetime) -> dict:
-	for d in dataset:
-		dataset[d] = { k:v for k, v in dataset[d].items() if k >= target }
-
-	all_keys = set()
-	for i in dataset:
-		all_keys.update(dataset[i].keys())
-
-	d = min(all_keys)
-	end = max(all_keys)
-	while d < end:
-		if d not in all_keys:
-			all_keys.add(d)
-		d = d + datetime.timedelta(minutes=1)
-
-	for i in all_keys:
-		for j in dataset:
-			if i not in dataset[j]: dataset[j][i]=0
-	return dataset
-
+        return __normalize_dataset(dataset, target, lambda d: d + datetime.timedelta(minutes=1) )
+    
 def plot(data:dict, name:str, kind, labels_filter:callable):
 	now = datetime.datetime.now()
 	timestamp = "{:04d}{:02d}{:02d}".format(now.year, now.month, now.day)
 	fn = "report_chart." + timestamp + "." + name.replace(' ', '-') + ".png"
 
-	chart = kind(width=2400, legend_at_bottom=True, x_label_rotation=45, show_minor_x_labels=False)
+	chart = kind(width=2400, legend_at_bottom=True, x_label_rotation=45, truncate_label=30)
 	chart.title = name
 
 	sd = set()
 	for k in data:
 		sd.update(data[k].keys())
-	sd = sorted(sd)
-	chart.x_labels_major = labels_filter(sd)
-	chart.x_labels = sd
-	if sd[0] not in chart.x_labels_major:
-		chart.x_labels_major.insert(0,sd[0])
-	if sd[-1] not in chart.x_labels_major:
-		chart.x_labels_major.append(sd[-1])
-
+	sd = sorted(labels_filter(sd))
+	chart.x_labels = [(str(d.date()) if 0 == d.hour and 0 == d.minute and 0 == d.second else "{:s}T{:s}".format("\u00A0"*20, str(d.time()))) for d in sd]
+	chart.x_labels[0] = str(sd[0])
+        
 	for k in sorted(data.keys()):
 		chart.add(k, [data[k][d] for d in sd if d in data[k]])
 	chart.render_to_png(fn, 120)
@@ -105,16 +87,18 @@ def main():
 	connect = cvs_load_connect("connect_log.hits-per-minute")
 	complaints = cvs_load_complaints("site_complaints_log.hits-per-hour")
 
-	all_keys = set()
+	all_complaints_keys = set()
 	for i in complaints:
-		all_keys.update(complaints[i].keys())
-	target = max(min(all_keys), min(connect.keys()))
+		all_complaints_keys.update(complaints[i].keys())
+	target_min = max(min(all_complaints_keys), min(connect.keys()))
+	target_max = min(max(all_complaints_keys), max(connect.keys()))
+	target_min = max(target_min, target_max - datetime.timedelta(days=8))
 
 	connections = dict()
 	connections['connections'] = connect
 
-	normalize_dataset_minutely(connections, target)
-	normalize_dataset_hourly(complaints, target)
+	normalize_dataset_minutely(connections, (target_min, target_max))
+	normalize_dataset_hourly(complaints, (target_min, target_max))
 
 	plot(connections, "Connections", pygal.Bar, lambda sd: [d for d in sd if 0 == d.minute and 0 == d.second])
 	plot(complaints, "Events", pygal.StackedBar, lambda sd: [d for d in sd if 0 == d.minute and 0 == d.second])
